8bab6fb3bd7ae0dfd087a987f123d797e07c6c32
umanagendrakumar <your.email@example.com>
2026-02-24 22:52:45 +0530

    Spell Corrected

--- src/store/appStore.js
+++ src/store/appStore.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import jobApplicationsReducer from "./jobAppSlice";
-import StatusHistoryReducer from "./statusHistorySlice";
+import StatusHistoryReducer from "./StatusHistorySlice";
 
 export const appStore = configureStore({
     reducer: {
