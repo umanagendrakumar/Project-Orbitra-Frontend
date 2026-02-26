bc48e8ce268808536ae526bc0f663c359b8b2738
umanagendrakumar <your.email@example.com>
2026-02-26 23:48:31 +0530

    AppRouter moved to other folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import JobApplicationDetails from './pages/JobApplicationDetails';
+import { useDispatch} from 'react-redux';
+
 import './App.css';
-import LoginPage from './pages/LoginPage';
-import HomePage from './pages/HomePage';
-import LandingPage from './pages/LandingPage';
-import RegisterPage from './pages/RegisterPage';
-import { use, useEffect } from 'react';
+
+import { useEffect } from 'react';
 import { setUser } from './store/userSlice';
 import { setJobApplications } from './store/jobAppSlice';
 import api from './utils/axios';
-import JobApplicationCU from './pages/JobApplicationCU';
-import { setStatusHistory } from './store/StatusHistorySlice';
+
+import AppRouter from './routes/AppRouter';
 
 const App = () => {
   const dispatch = useDispatch();
-
 
   const initializeApp = async () => {
     try {
@@ -34,28 +29,11 @@ const App = () => {
 
   useEffect(() => {
     initializeApp();
-
   }, []);
 
   return (
     <AppRouter />
   );
-}
-
-const AppRouter = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/register' element={<RegisterPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/orbit' element={<HomePage />} />
-        <Route path='/job-application/post' element={<JobApplicationCU isEdit={false} />} />
-        <Route path='/job-application/patch/:jobId' element={<JobApplicationCU isEdit={true} />} />
-        <Route path='/job-application/get/:jobId' element={<JobApplicationDetails />} />
-      </Routes>
-    </BrowserRouter>
-  )
 }
 
 export default App;
