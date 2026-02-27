81b8bd8b0fe302abd7e884643e94d00ca2fed641
umanagendrakumar <your.email@example.com>
2026-02-27 14:34:14 +0530

    format changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 const Footer = () => {
+  const startYear = 2026;
+  const currentYear = new Date().getFullYear();
   return (
-    <footer className="text-center py-6 text-gray-500 text-sm bg-white">
-      © {new Date().getFullYear()} Orbitra KunK. All rights reserved.
+    <footer className="text-center py-6 text-gray-500 text-sm bg-white shadow-[0_-1px_3px_rgba(0,0,0,0.1)]">
+      © {startYear}{currentYear > startYear && `-${currentYear}`} Kambala Uma Nagendra Kumar. All rights reserved.
     </footer>
   );
 };
