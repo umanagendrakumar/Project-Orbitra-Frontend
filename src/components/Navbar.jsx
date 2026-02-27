41e4d577ecd0cd90ea2848be8a46da581344313e
umanagendrakumar <your.email@example.com>
2026-02-27 23:38:48 +0530

    icon and profile side bar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,39 @@
-import { useDispatch, useSelector } from "react-redux";
-import api from "../utils/axios";
-import { clearUser } from "../store/userSlice";
-import { useNavigate } from "react-router-dom";
-import { clearJobApplications } from "../store/jobAppSlice";
+import { useSelector } from "react-redux";
+import Logout from "./Logout";
+import { useState } from "react";
+import { RiMenuUnfold2Line } from "react-icons/ri";
+import { RiMenuUnfoldLine } from "react-icons/ri";
 
 const Navbar = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
   const user = useSelector((store) => store.user);
 
-  const handleSignOut = async () => {
-    try {
-      await api.post("/logout");
-      dispatch(clearUser());
-      dispatch(clearJobApplications());
-      navigate("/");
-    } catch (error) {
-      console.error("Error signing out:", error.response);
-    }
-  }
+  const [profileVisible, setProfileVisible] = useState(false);
 
   return (
-    <nav className="sm:px-8 px-4 py-4 shadow-sm bg-white flex justify-between">
+    <nav className="sm:px-8 px-4 py-4 shadow-sm bg-white flex justify-between items-center">
       <h1 className="text-3xl font-bold text-purple-600">Orbitra</h1>
-      {user && (
-        <button
-          onClick={handleSignOut}
-          className=" sm:text-lg text-sm font-medium text-red-500 cursor-pointer hover:text-red-700 transition"
-        >
-          Leave Orbit
-        </button>
-      )
+      {
+        <div className="relative">
+          <button
+            className="text-2xl cursor-pointer p-2"
+            onClick={() => setProfileVisible(!profileVisible)}
+          >
+            {profileVisible ? <RiMenuUnfold2Line /> : <RiMenuUnfoldLine />}
+          </button>
+
+          <div
+            className={`absolute right-0 mt-2 bg-white shadow-md flex flex-col text-left p-4 z-50 ${profileVisible ? "block" : "hidden"
+              }`}
+          >
+            <h1 className="font-semibold">
+              {user?.lastName} {user?.firstName}
+            </h1>
+            <h1 className="text-sm text-gray-500 mb-4">
+              {user?.email}
+            </h1>
+            <Logout />
+          </div>
+        </div>
       }
     </nav>
   );
